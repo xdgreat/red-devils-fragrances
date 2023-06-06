6d05aaeb633d2a6095fdf87693aa5d8de78de01c
nik <nnaiker01@gmail.com>
2023-06-06 23:27:35 +1200

    added new images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function invertColor() {
-  let navElement = document.querySelector("nav");
-  let icons = document.querySelectorAll(".nav-icons");
-  this.scrollY > 800
+  const navElement = document.querySelector("nav");
+  const icons = document.querySelectorAll(".nav-icons");
+  this.scrollY > 700
     ? (navElement.classList.remove("transparent"),
       icons.forEach((icon) => {
         icon.style.filter = "invert(0%)";
@@ -10,7 +10,55 @@ function invertColor() {
       icons.forEach((icon) => {
         icon.style.filter = "invert(100%)";
       }));
-      console.log("aa")
 }
 
 window.addEventListener("scroll", invertColor, false);
+
+const slideEl = document.querySelectorAll(".slide");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("showX", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+slideEl.forEach((slide) => {
+  observer.observe(slide);
+});
+
+const product = document.querySelectorAll(".product-container");
+
+const productObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("showZ", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+product.forEach((slide) => {
+  productObserver.observe(slide);
+});
+
+
+const items = [
+  {
+    test:"ahah",
+    lmfao:45
+  },
+  {
+    testy:"asd",
+    adsa:76
+  }
+]
+
+localStorage.setItem("test", items)
+console.log(items)
